9055ee0b82348f625e372187a250cfa0d1929855
AshadAhmad <ashadahmad12@gmail.com>
2020-04-22 13:01:20 +0530

    Updates

---
+++ precache-manifest.ea31c2610375012bad84c435f7e8b191.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a340bd10b7d5cb89c97d85d8348166",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "1ffef73b7b82eee23d16",
+    "url": "/contactmanager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ddf6d52b9cd5613a77fd",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "1ffef73b7b82eee23d16",
+    "url": "/contactmanager/static/js/2.dd343d90.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.dd343d90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf6d52b9cd5613a77fd",
+    "url": "/contactmanager/static/js/main.47155576.chunk.js"
+  },
+  {
+    "revision": "98d49c782551fbed3aca",
+    "url": "/contactmanager/static/js/runtime-main.53501af3.js"
+  }
+]);
